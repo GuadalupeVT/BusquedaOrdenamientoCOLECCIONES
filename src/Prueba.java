73896086fe6c2d47c51b194e87914508236a06cc
Guadalupe Vazquez <guvadlt@gmail.com>
2018-12-05 08:23:21 -0600

    Se agrego teoria

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,8 +1,27 @@
 
+/*
+ * Framework coleciones Java
+ * 
+ * INTERFACES
+ *     Set    NO permite duplicados
+ *     List   Si permite duplicados
+ *     Map    No permite duplicados de CLAVES (Pares CLAVE-VALOR)
+ *     
+ *     Implementaciones
+ *         Set  --> HashSet, LinkedHashSet y Treeset
+ *         List --> ArrayList y LinkedList
+ *         Map  --> HashMap, LinkedHashMap y TreeMap
+ *         
+ * CLASES
+ *     Vector
+ *     Que
+ *     Deque
+ *     Stack
+ *   
+ */
 public class Prueba {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
